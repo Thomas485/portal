1f07dfc9cdad49149a955aea1a5bf025e6df1e2e
Thomas485 <1681511+Thomas485@users.noreply.github.com>
2023-09-27 14:55:03 +0200

    refactor

---
+++ config.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type Route struct {
+	Source string `json:"source"`
+	Dest   string `json:"dest"`
+}
+
+type Config struct {
+	File   string `json:"-"`
+	Port   int
+	Routes []Route
+}
+
+func (c *Config) Serve(cert, key string) error {
+	proxyHandler := func(w http.ResponseWriter, r *http.Request) {
+		source := strings.ToLower(r.Host)
+
+		dest, err := findDestination(c.Routes, source)
+		if err != nil {
+			http.Error(w, "Host not found", http.StatusNotFound)
+			return
+		}
+
+		target, err := url.Parse(dest)
+		if err != nil {
+			http.Error(w, "Invalid URL format", http.StatusInternalServerError)
+			return
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(target)
+		proxy.ServeHTTP(w, r)
+	}
+
+	if cert != "" && key != "" {
+		return http.ListenAndServeTLS(fmt.Sprintf(":%d", c.Port), cert, key, http.HandlerFunc(proxyHandler))
+	} else {
+		return http.ListenAndServe(fmt.Sprintf(":%d", c.Port), http.HandlerFunc(proxyHandler))
+	}
+}
+
+func findDestination(routes []Route, source string) (string, error) {
+	for _, route := range routes {
+		if route.Source == source {
+			return route.Dest, nil
+		}
+	}
+	return "", errors.New("route not found")
+}
+
+func loadConfig(filepath string) (*Config, error) {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	config.File = filepath
+
+	return &config, nil
+}
+
+func (c Config) SaveToFile(filepath string) error {
+	data, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filepath, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,51 +1,34 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/urfave/cli"
 )
 
-type Route struct {
-	Source string `json:"source"`
-	Dest   string `json:"dest"`
-}
-
-type Config struct {
-	File   string `json:"-"`
-	Port   int
-	Routes []Route
-}
-
-var flags = []cli.Flag{
-	&cli.StringFlag{
-		Name:      "file",
-		Value:     "config.json",
-		Usage:     "the configuration file",
-		TakesFile: true,
-	},
-	&cli.StringFlag{
-		Name:      "cert",
-		Value:     "",
-		Usage:     "the tls certificate",
-		TakesFile: true,
-	},
-	&cli.StringFlag{
-		Name:      "key",
-		Value:     "",
-		Usage:     "the public key of the certificate",
-		TakesFile: true,
-	},
-}
-
 func main() {
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:      "file",
+			Value:     "config.json",
+			Usage:     "the configuration file",
+			TakesFile: true,
+		},
+		&cli.StringFlag{
+			Name:      "cert",
+			Value:     "",
+			Usage:     "the tls certificate",
+			TakesFile: true,
+		},
+		&cli.StringFlag{
+			Name:      "key",
+			Value:     "",
+			Usage:     "the public key of the certificate",
+			TakesFile: true,
+		},
+	}
+
 	app := &cli.App{
 		Name: "portal",
 		Commands: []cli.Command{
@@ -108,42 +91,6 @@ func interactive(c *cli.Context) error {
 	return config.Serve(cert, key)
 }
 
-func findDestination(routes []Route, source string) (string, error) {
-	for _, route := range routes {
-		if route.Source == source {
-			return route.Dest, nil
-		}
-	}
-	return "", errors.New("route not found")
-}
-
-func (c *Config) Serve(cert, key string) error {
-	proxyHandler := func(w http.ResponseWriter, r *http.Request) {
-		source := strings.ToLower(r.Host)
-
-		dest, err := findDestination(c.Routes, source)
-		if err != nil {
-			http.Error(w, "Host not found", http.StatusNotFound)
-			return
-		}
-
-		target, err := url.Parse(dest)
-		if err != nil {
-			http.Error(w, "Invalid URL format", http.StatusInternalServerError)
-			return
-		}
-
-		proxy := httputil.NewSingleHostReverseProxy(target)
-		proxy.ServeHTTP(w, r)
-	}
-
-	if cert != "" && key != "" {
-		return http.ListenAndServeTLS(fmt.Sprintf(":%d", c.Port), cert, key, http.HandlerFunc(proxyHandler))
-	} else {
-		return http.ListenAndServe(fmt.Sprintf(":%d", c.Port), http.HandlerFunc(proxyHandler))
-	}
-}
-
 func tlsCerts(c *cli.Context) (string, string, error) {
 	var cert = c.String("cert")
 	var key = c.String("key")
@@ -162,35 +109,4 @@ func tlsCerts(c *cli.Context) (string, string, error) {
 
 	// https
 	return cert, key, nil
-}
-
-func loadConfig(filepath string) (*Config, error) {
-	data, err := os.ReadFile(filepath)
-	if err != nil {
-		return nil, err
-	}
-
-	var config Config
-	err = json.Unmarshal(data, &config)
-	if err != nil {
-		return nil, err
-	}
-
-	config.File = filepath
-
-	return &config, nil
-}
-
-func (c Config) SaveToFile(filepath string) error {
-	data, err := json.MarshalIndent(c, "", "    ")
-	if err != nil {
-		return err
-	}
-
-	err = os.WriteFile(filepath, data, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
